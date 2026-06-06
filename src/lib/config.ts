0983700c34b47a70a7ee176c062e406483f10c7b
Yulanto-Developers <yulantodevelopers@gmail.com>
2026-06-06 14:55:05 +0530

    port chnaged

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,6 +2,7 @@ import mysql from 'mysql2/promise'
 
 export const db = mysql.createPool({
     host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT) || 3306,
     user: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB,
