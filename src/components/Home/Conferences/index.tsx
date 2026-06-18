e41fc8336d8e54bd0ae7564b7c218691df55709a
Yulanto-Developers <yulantodevelopers@gmail.com>
2026-06-18 15:33:42 +0530

    chnges

--- src/components/Home/Conferences/index.tsx
+++ src/components/Home/Conferences/index.tsx
@@ -5,7 +5,7 @@ import ThumbnailCarousel from "../ThumbnailCarousel";
 const Conferences = () => {
   return (
     <>
-      <section className="bg-IcyBreeze dark:bg-[#fff] relative overflow-hidden before:absolute before:content-[''] before:bg-PaleSkyBlu before:dark:bg-[#f1671a] before:w-687 before:h-687 before:-bottom-1/2 before:rounded-full before:xl:inline-block before:hidden">
+      <section className="bg-IcyBreeze dark:bg-[#fff] relative overflow-hidden before:absolute before:content-[''] before:bg-PaleSkyBlu before:dark:bg-[#0A2947] before:w-687 before:h-687 before:-bottom-1/2 before:rounded-full before:xl:inline-block before:hidden">
         <div className="container">
           <div className="grid md:grid-cols-2 grid-cols-1 items-center lg:gap-24 gap-5">
             <div
@@ -21,7 +21,7 @@ const Conferences = () => {
               data-aos-delay="200"
               data-aos-duration="1000"
             >
-              <span className="uppercase text-[11px] font-black italic px-2 py-1 bg-[#58595B] rounded-xl tracking-wider text-[#fff]">Welcome to </span>
+              <span className="uppercase text-[11px] font-black italic px-2 py-1 bg-[#58595B]/50 rounded-xl tracking-wider text-[#fff]">Welcome to </span>
               <h2 className="uppercase text-[#F1671A]">freight digital</h2>
               <p className="text-lg font-normal text-justify text-[#58595B] dark:text-opacity-90 max-w-506 md:pt-3 pt-5 md:pb-10 pb-6">
                 Freight Digital is a specialized digital marketing partner  dedicated exclusively to the freight forwarding, logistics, shipping, and supply chain industry. We help freight forwarders, customs brokers, NVOCCs, transport operators, and logistics service providers strengthen their digital presence, enhance brand visibility, and generate new business opportunities worldwide.
